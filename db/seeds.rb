ab408b9a304b2f174c533f9ca7801b76afc050fa
maxime ayllon <maximeayllon12@gmail.com>
2021-02-25 16:22:25 +0100

    back-end finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
-puts 'Destroying previous seeds'
+require 'json'
+require 'open-uri'
+
+puts 'Destroying (I) previous seeds'
 Ingredient.destroy_all
 
 puts "Creation of new seeds"
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+filepath = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+serialized_ingredients = open(filepath).read
+ingredients = JSON.parse(serialized_ingredients)
+
+ingredients.each do |ingredient|
+  ingredient[1].each do |i|
+    Ingredient.create(name: i["strIngredient1"])
+  end
+end
 
 puts "Seeds created"
