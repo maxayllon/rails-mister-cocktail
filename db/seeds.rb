faf33fbf7a6574ba496c8af1b9760198887358a7
maxime ayllon <maximeayllon12@gmail.com>
2021-02-26 15:47:33 +0100

    navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,15 @@ ingredients.each do |ingredient|
 end
 
 puts "Seeds created"
+
+
+puts 'Destroying (C) previous seeds'
+Cocktail.destroy_all
+
+
+puts "other Seeds"
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+cocktail = Cocktail.create(name: 'Mojito')
+cocktail.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
+puts "New Seeds created"
